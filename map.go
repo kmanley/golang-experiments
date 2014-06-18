5b22f6efb6bd83235cd1c34b719709d6a7564221
Kevin Manley <kevin.manley+github@gmail.com>
2014-06-17 22:03:30 -0400

    more experimenting

--- map.go
+++ map.go
@@ -1,24 +1,26 @@
 package main
 
 import "fmt"
+
 var print = fmt.Println
 
 func main() {
-  people := make(map[string]int)
-  people["Kevin"] = 46
-  people["Magnus"] = 6
-  people["Thor"] = 4
-  people["Amy"] = 42
-  print(people)
-  //print(people.keys())
-  age, ok := people["Kevin"]
-  print(age, ok)
-  age, ok = people["Fred"]
-  print(age, ok)
-  if age, ok = people["Magnus"]; ok {
-     print("what the f?")
-  }
+	people := make(map[string]int)
+	people["Kevin"] = 46
+	people["Magnus"] = 6
+	people["Thor"] = 4
+	people["Amy"] = 42
+	print(people)
+	//print(people.keys())
+	age, ok := people["Kevin"]
+	print(age, ok)
+	age, ok = people["Fred"]
+	print(age, ok)
+	if age, ok = people["Magnus"]; ok {
+		print(age)
+	}
+	for key, val := range people {
+		print(key, val)
+	}
 
-  z := 3;4
-  print(z)
 }
