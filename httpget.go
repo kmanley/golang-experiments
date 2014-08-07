ed5b62da0d78f5d65da4ed9fc21b3704253c8d42
Kevin Manley <kevin.manley+github@gmail.com>
2014-08-07 19:00:52 -0400

    test persistent client with manners

---
+++ httpget.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main1() {
+	for {
+		url := "localhost:8080/foo"
+		fmt.Println("getting", url)
+		http.Get(url)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	client := &http.Client{}
+	ctr := 0
+	for {
+		url := "http://localhost:8080/foo"
+		fmt.Println(ctr, "getting", url)
+		resp, err := client.Get(url)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			bytes, _ := ioutil.ReadAll(resp.Body)
+			fmt.Println(string(bytes))
+			resp.Body.Close()
+		}
+		time.Sleep(1 * time.Second)
+		ctr += 1
+	}
+}
