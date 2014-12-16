bfc1104d31f948d8003ce57cb9f4c27526dcaf9e
Kevin Manley <kevin.manley+github@gmail.com>
2014-12-15 21:15:44 -0500

    more bolt experiments

--- test_boltdb.go
+++ test_boltdb.go
@@ -1,20 +1,83 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	_ "fmt"
 	"github.com/boltdb/bolt"
 	"log"
+	"time"
 )
+
+type JobControl struct {
+	MaxConcurrency         uint32
+	StartTime              time.Time
+	ContinueJobOnTaskError bool
+	RemoteCurDir           string
+	WorkerNameRegex        string
+	//	CompiledWorkerNameRegex *regexp.Regexp
+	// TODO: consider OSRegex as well, to limit to Workers matching a particular OS/version
+	ProcessPriority int
+	// TODO: later
+	//AssignSingleTaskPerWorker bool
+	//TaskWorkerAssignment      map[string][]uint32
+	JobPriority          int8    // higher value means higher priority
+	JobTimeout           float64 // seconds
+	TaskTimeout          float64 // seconds
+	TaskSeemsHungTimeout uint32
+	AbandonedJobTimeout  uint32
+	MaxTaskReallocations uint8
+}
+
+func (this *JobControl) ToBytes() ([]byte, error) {
+	var buff bytes.Buffer
+	enc := gob.NewEncoder(&buff)
+	err := enc.Encode(this)
+	if err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
+}
+
+func (this *JobControl) FromBytes(data []byte) error {
+	buff := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buff)
+	err := dec.Decode(this)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+func JobControlFromBytes(data []byte) (*JobControl, error) {
+	buff := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buff)
+	ctrl := &JobControl{}
+	err := dec.Decode(ctrl)
+	if err != nil {
+		return nil, err
+	}
+	return ctrl, nil
+}
+*/
 
 func DoUpdate(db *bolt.DB) error {
 	err := db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucket([]byte("MyBucket"))
+		b, err := tx.CreateBucketIfNotExists([]byte("MyBucket"))
 		if err != nil {
 			return err
 		}
 
-		err = b.Put([]byte("answer"), []byte("42"))
+		ctrl := &JobControl{MaxConcurrency: 316, StartTime: time.Now(), ContinueJobOnTaskError: true, RemoteCurDir: "/some/dir"}
+		data, err := ctrl.ToBytes()
 		if err != nil {
+			log.Println("err: ", err)
+			return err
+		}
+		err = b.Put([]byte("answer"), data)
+		if err != nil {
+			log.Println("err: ", err)
 			return err
 		}
 		return nil
@@ -32,10 +95,18 @@ func DoRead(db *bolt.DB) error {
 		//}
 
 		v := b.Get([]byte("answer"))
+		//log.Printf("v=%s", v)
 		//if err != nil {
 		//	return err
 		//}
-		log.Printf("the value is %s", v)
+		//ctrl, err := JobControlFromBytes(v)
+		ctrl := &JobControl{}
+		err := ctrl.FromBytes(v)
+		if err != nil {
+			log.Println("error", err)
+			return err
+		}
+		log.Printf("the value is %+v", ctrl)
 		return nil
 	})
 	return err
@@ -51,7 +122,7 @@ func main() {
 	}
 	defer db.Close()
 
-	//DoUpdate(db)
+	DoUpdate(db)
 	DoRead(db)
 
 }
