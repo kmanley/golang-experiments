857ef7a3460a271c04b08d9ce817e783edb88b3c
kevin <kevin.manley+github@gmail.com>
2018-01-05 15:49:38 -0500

    testing

--- exigo/main.go
+++ exigo/main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/gcmurphy/getpass"
-	"github.com/kmanley/golang-experiments/exigo/myservice"
+	"github.com/golang/glog"
+	exigo "github.com/kmanley/go-exigo-client"
 )
 
-func main() {
-	//url := "https://v5live4.exigo.com/admin6/fb6632a52597447d8a986e4dde101ca4/"
-	url := "http://api.exigo.com/3.0/"
-	username := "kevinm"
-	password, _ := getpass.GetPass()
-	auth := &myservice.BasicAuth{username, password}
-	client := myservice.NewExigoApiSoap(url, true, auth)
-	client.AddHeader(&myservice.ApiAuthentication{LoginName: username, Password: password, Company: "vivnetwork"})
-	req := &myservice.GetCustomersRequest{CustomerID: 4}
-	res, err := client.GetCustomers(req)
+func testGetCustomer(client *exigo.Client) {
+	res, err := client.GetCustomerByID(14)
 	if err != nil {
-		fmt.Println("error: %s", err)
+		glog.Errorf("GetCustomerByID error: %s", err)
+	} else {
+		glog.Info("success")
+		spew.Dump(res)
+	}
+}
+
+/*
+func testCreateCustomer(client *exigo.ExigoApiSoap) {
+	req := &exigo.CreateCustomerRequest{
+		CustomerType: 1,
+		//FirstName: "Alfred",
+		//LastName:  "Ignatius",
+		Email: "alfred@aol.com",
+		Phone: "203-555-1212",
+		Notes: "Created via API",
+	}
+
+	res, err := client.CreateCustomer(req)
+	spew.Dump(res)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
 		return
 	}
 	fmt.Println("success!")
-	spew.Dump(res)
+
+}
+*/
+
+func main() {
+	//DT1 := time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC)
+	//DT2 := time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
+	//url := "https://v5live4.exigo.com/admin6/fb6632a52597447d8a986e4dde101ca4/"
+	client := exigo.NewClient("", "kevinm", os.Getenv("kpass"), "vivnetwork")
+	testGetCustomer(client)
+	//testCreateCustomer(client)
 }
